169ea718a61edd4474e0ca109344b9e5af49d12c
tvisha9 <74127814+tvisha9@users.noreply.github.com>
2024-06-17 11:29:19 -0700

    added the delete route

--- server.js
+++ server.js
@@ -65,6 +65,35 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id; // Get the note ID from the URL parameter
+    fs.readFile(dbPath, (err, data) => {  //read the db.jason file
+      if (err) {  //error handling
+        console.log(err);
+        res.status(500).json('Error reading db.json');
+      } else {
+        const notes = JSON.parse(data);   //parse the JSON data from file
+        const noteToDelete = notes.find((note) => note.id === id);
+        if (!noteToDelete) {
+          res.status(404).json('Note not found');
+        } else {
+          notes.splice(notes.indexOf(noteToDelete), 1);
+          fs.writeFile(dbPath, JSON.stringify(notes), (err) => {
+            if (err) {
+              console.log(err);
+              res.status(500).json('Error writing to db.json');
+            } else {
+              res.status(204).json({ // 204 status stands for no content
+                status: 'success',
+                data: null
+              });
+            }
+          });
+        }
+      }
+    });
+  });
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
